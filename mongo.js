699e22e73d0ad4526c481f68186103c8aaecb22b
Gammaa400 <87341703+Gammaa400@users.noreply.github.com>
2021-07-12 17:32:53 -0600

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose')
 
 const mongoPath =
-  'mongodb+srv://OL:qOnouQUqK2SFmgBq@mongodb-albion.l7vuv.mongodb.net/MongoDB-Albion?retryWrites=true&w=majority&connectTimeoutMS=10000'
+  ''
 
 module.exports = async () => {
   await mongoose.connect(mongoPath, {
